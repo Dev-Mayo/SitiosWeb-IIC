c63223cba0ce8432fa5bdb22250d7164416d9bed
dvid <DAVIDCARRANZAG07@GMAIL.COM>
2026-05-31 19:35:10 -0600

    aaa

--- ADMExpedientePersonal/ADMExpedientePersonal.Entities/Concurso.cs
+++ ADMExpedientePersonal/ADMExpedientePersonal.Entities/Concurso.cs
@@ -10,6 +10,7 @@ namespace ADMExpedientePersonal.Entities
     {
         public string codigo_concurso { get; set; }
         public string nombre { get; set; }
+
         public DateTime fecha_inicio { get; set; }
         public DateTime fecha_fin { get; set; }
         public string estado { get; set; }
