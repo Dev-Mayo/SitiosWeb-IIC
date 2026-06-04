b20efcac545ca2811b6dc75efc9f9524faa2a157
IMSTG94 <steveng316.sgs@gmail.com>
2026-06-03 21:02:04 -0600

    EMP5 Administracion de Acciones de Personal

---
+++ ADMExpendientePersonalRazor/AdminPersonalWebCore/AdminPersonalWebCore.Entities/AccionPersonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPersonalWebCore.Entities
+{
+    public class AccionPersonal
+    {
+        public int AccionId { get; set; }
+        public int CodigoAccion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public int EmpleadoId { get; set; }
+        public int JefaturaId { get; set; }
+
+        public string? NombreEmpleado { get; set; }
+        public string? NombreJefatura { get; set; }
+    }
+}
--- ADMExpendientePersonalRazor/AdminPersonalWebCore/AdminPersonalWebCore/Program.cs
+++ ADMExpendientePersonalRazor/AdminPersonalWebCore/AdminPersonalWebCore/Program.cs
@@ -32,9 +32,9 @@ builder.Services.AddSingleton<BitacoraRepository>(_ => new BitacoraRepository(bi
 builder.Services.AddSingleton<AdminUsuarioRepository>(_ => new AdminUsuarioRepository(segConn));
 builder.Services.AddSingleton<InstEducativaRepository>(_ => new InstEducativaRepository(genConn));
 builder.Services.AddSingleton<RequisitoPuestoRepository>(_ => new RequisitoPuestoRepository(empConn)); //emp3
-builder.Services.AddScoped<AreaRepository>(sp =>new AreaRepository(empConn)); //emp4
+builder.Services.AddSingleton<AreaRepository>(_ => new AreaRepository(empConn)); // EMP4
+builder.Services.AddSingleton<AccionPersonalRepository>(_ => new AccionPersonalRepository(empConn)); // EMP5
 
-builder.Services.AddScoped<AreaService>();
 
 // Services (BLL) 
 builder.Services.AddSingleton<BitacoraService>();
