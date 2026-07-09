01718f88f4ea652ecf21169a777540d85317c55f
IMSTG94 <steveng316.sgs@gmail.com>
2026-07-08 18:47:59 -0600

    Agrega estructura base del proyecto Servicios Web Core

---
+++ ServiciosWebCore/ADMExpedientePersonal.WebServices/Data/IDbConnectionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace ADMExpedientePersonal.WebServices.Data
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection CreateConnection(string connectionName);
+    }
+}
