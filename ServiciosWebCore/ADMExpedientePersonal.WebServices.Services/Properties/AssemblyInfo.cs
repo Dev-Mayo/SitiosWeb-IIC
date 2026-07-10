c574fbd17254615b334647bc576b0abdea5aecd8
IMSTG94 <steveng316.sgs@gmail.com>
2026-07-09 18:22:28 -0600

    Integra cambios de ServiciosWebCore desde Steven-Core

---
+++ ServiciosWebCore/ADMExpedientePersonal.WebServices.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("ADMExpedientePersonal.WebServices.Services")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADMExpedientePersonal.WebServices.Services")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d78697f8-7e91-46c5-95ac-b0e9d54f165e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ServiciosWebCore/ADMExpedientePersonal.WebServices/EmpleadoService.svc.cs
+++ ServiciosWebCore/ADMExpedientePersonal.WebServices/EmpleadoService.svc.cs
@@ -2,71 +2,31 @@
 using ADMExpedientePersonal.WebServices.Contracts;
 using ADMExpedientePersonal.WebServices.Entities.Requests;
 using ADMExpedientePersonal.WebServices.Entities.Responses;
-using ADMExpedientePersonal.WebServices.Repositories;
 using System;
+
+// Alias para evitar conflicto con el nombre, porque se llama igual al que tengo en el proyecto.services
+using EmpleadoBusinessService = ADMExpedientePersonal.WebServices.Services.EmpleadoService;
 
 namespace ADMExpedientePersonal.WebServices
 {
     public class EmpleadoService : IEmpleadoService
     {
-        public EmpleadoResponse RegistrarEmpleado(EmpleadoRequest request)
-        {
-            try
-            {
-                if (request == null)
-                    return Error("No se recibieron datos del empleado.");
+        private readonly EmpleadoBusinessService _service;
 
-                if (string.IsNullOrWhiteSpace(request.Identificacion))
-                    return Error("La identificación es requerida.");
-
-                if (string.IsNullOrWhiteSpace(request.TipoIdentificacion))
-                    return Error("El tipo de identificación es requerido.");
-
-                if (string.IsNullOrWhiteSpace(request.NombreCompleto))
-                    return Error("El nombre completo es requerido.");
-
-                if (request.FechaNacimiento == default(DateTime))
-                    return Error("La fecha de nacimiento es requerida.");
-
-                if (request.PuestoId <= 0)
-                    return Error("El puesto es requerido.");
-
-                if (request.Telefonos == null || request.Telefonos.Count == 0)
-                    return Error("Debe indicar al menos un teléfono.");
-
-                var repository = RepositoryFactory.CrearEmpleadoRepository();
-
-                if (repository.ExisteEmpleado(request.Identificacion))
-                    return Error("Ya existe un empleado con esa identificación.");
-
-                int empleadoId = repository.RegistrarEmpleado(request);
-
-                return new EmpleadoResponse
-                {
-                    Exito = true,
-                    Mensaje = "Empleado creado con éxito.",
-                    EmpleadoId = empleadoId
-                };
-            }
-            catch (Exception ex)
-            {
-                return Error("Error técnico al registrar el empleado: " + ex.Message);
-            }
-        }
-
-        private EmpleadoResponse Error(string mensaje)
-        {
-            return new EmpleadoResponse
-            {
-                Exito = false,
-                Mensaje = mensaje,
-                EmpleadoId = 0
-            };
-        }
-        public int ProbarConexionEMP()
+        public EmpleadoService()
         {
             var repository = RepositoryFactory.CrearEmpleadoRepository();
-            return repository.ProbarConexionEMP();
+            _service = new EmpleadoBusinessService(repository);
+        }
+
+        public EmpleadoResponse RegistrarEmpleado(EmpleadoRequest request)
+        {
+            return _service.RegistrarEmpleado(request);
+        }
+
+        public int ProbarConexionEMP()
+        {
+            return _service.ProbarConexionEMP();
         }
     }
 }
