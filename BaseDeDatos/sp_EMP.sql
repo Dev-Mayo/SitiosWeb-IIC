e2339473d0fe06f86dff06e00fee62c0a722a1a9
RafaMarin835 <85074741+RafaMarin835@users.noreply.github.com>
2026-07-20 00:11:02 -0600

    Update sp_EMP.sql

--- BaseDeDatos/sp_EMP.sql
+++ BaseDeDatos/sp_EMP.sql
@@ -13,19 +13,32 @@ BEGIN
 END$$
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS sp_listar_puestos_disponibles;
+
+
+
+
+
+
+DROP PROCEDURE IF EXISTS EMP.sp_listar_puestos_disponibles;
+
 DELIMITER $$
-CREATE PROCEDURE sp_listar_puestos_disponibles()
+CREATE PROCEDURE EMP.sp_listar_puestos_disponibles()
 BEGIN
-    SELECT
-    p.puesto_id, p.nombre, p.salario,
-    jefe_emp.nombre_completo AS nombre_jefe,
-    p.disponible
-FROM puestos p
-LEFT JOIN empleados jefe_emp ON jefe_emp.puesto_id = p.jefe_puesto_id
-WHERE p.disponible = 1
-ORDER BY p.nombre;
+    SELECT DISTINCT
+        p.puesto_id,
+        p.nombre,
+        p.salario,
+        COALESCE(j.nombre_completo, 'Sin asignar') AS nombre_jefe,
+        p.disponible
+    FROM EMP.puestos p
+    LEFT JOIN EMP.empleados j
+        ON j.empleado_id = p.jefe_puesto_id
+    INNER JOIN OFE.concursos c
+        ON c.puesto_id = p.puesto_id
+    WHERE p.disponible = 1
+      AND c.estado = 'Vigente'
+      AND CURDATE() BETWEEN c.fecha_inicio AND c.fecha_fin
+    ORDER BY p.nombre;
 END$$
 DELIMITER ;
-
 
