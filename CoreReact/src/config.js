c92bbc1ce7df982605484e651a97b0e6b5525ab8
RafaMarin835 <85074741+RafaMarin835@users.noreply.github.com>
2026-08-11 16:11:50 -0600

    Update config.js

--- CoreReact/src/config.js
+++ CoreReact/src/config.js
@@ -12,7 +12,8 @@
 // "Authorization: Bearer <token>" en el resto de llamadas.
 // ---------------------------------------------------------------------------
 
-export const API_BASE_URL = 'https://sitiosweb-iic-3.onrender.com';
+//export const API_BASE_URL = 'https://sitiosweb-iic-3.onrender.com';
+export const API_BASE_URL = 'https://admexpedientepersonalapigateway20260809214543.azurewebsites.net/';
 
 // Claves de almacenamiento local (sesión).
 export const USER_KEY = 'corereact_usuario';
